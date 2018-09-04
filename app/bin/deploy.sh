eff20383e7cf48d3c35b1b4c917bba1a57e6243b
wujiaming <wujiaming@iplas.com.cn>
2018-09-04 10:23:34 +0800

    push

--- app/bin/deploy.sh
+++ app/bin/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker cp ~/Work/Jimmy/nginxLua/app openresty:/usr/local/
-docker cp ~/Work/Jimmy/nginxLua/nginx_conf/nginx.conf openresty:/usr/local/openresty/nginx/conf/
+docker cp ../../../app openresty:/usr/local/
+docker cp ../../../nginx_conf/nginx.conf openresty:/usr/local/openresty/nginx/conf/
 
 docker exec -it openresty /bin/sh /usr/local/app/bin/reload.sh
