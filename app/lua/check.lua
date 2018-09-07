6e739ce41c3ee798a2f76a39689029d4dba87531
wujiaming <wujiaming@iplas.com.cn>
2018-09-07 16:45:51 +0800

    push

--- app/lua/check.lua
+++ app/lua/check.lua
@@ -4,7 +4,8 @@ local staffId = ngx.var.cookie_StaffId
 local s_value = sharedDb:get(staffId)
 
 if s_value ~= nil and "gray" == s_value then
-    return ngx.location.capture("/gray")
+    ngx.say("gray")
+    return
 end
-
-return ngx.location.capture("/product")
+ngx.say("product")
+return
