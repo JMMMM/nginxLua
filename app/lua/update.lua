70be0e818e0697e7b41e8e1022d2cbdafbac1da7
wujiaming <wujiaming@iplas.com.cn>
2018-09-07 15:04:26 +0800

    push

--- app/lua/update.lua
+++ app/lua/update.lua
@@ -1,11 +1,11 @@
 local redis_pool = require("redis_pool")
 
-local client = redis_pool:new('192.168.4.196', '6379', 5000)
+local pool = redis_pool:new('192.168.4.196', '6379', 5000)
 local ok, err = redis_pool:get_connect()
 if not ok then ngx.log(ngx.ERR, err) end
 
-local resp, err = client:lrange("gray_list", 0, -1)
-redis_pool.return_connect(client)
+local resp, err = pool.client:lrange("gray_list", 0, -1)
+redis_pool:return_connect()
 
 local sharedDb = ngx.shared.shared_dict
 sharedDb:set('gray_list', resp)
