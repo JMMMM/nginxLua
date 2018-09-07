c731d00f2e5c993dc1c7eeae7c4d0ff61e4a966d
wujiaming <wujiaming@iplas.com.cn>
2018-09-07 16:29:12 +0800

    push

--- app/lua/check.lua
+++ app/lua/check.lua
@@ -1,11 +1,10 @@
 local sharedDb = ngx.shared.shared_dict
-local gray_list = sharedDb:get("gray_list")
 
 local staffId = ngx.var.cookie_StaffId
-local s_value = gray_list[staffId]
+local s_value = sharedDb:get(staffId)
 
 if s_value ~= nil and "gray" == s_value then
-    return ngx.redirect("/gray")
+    return ngx.location.capture("/gray")
 end
 
-return ngx.redirect("/product")
+return ngx.location.capture("/product")
